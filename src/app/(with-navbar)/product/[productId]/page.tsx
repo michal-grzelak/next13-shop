f9f79eaa625fdd02e39cc198f9dd1a2b86c10a19
Michał Grzelak <grzelakm@outlook.com>
2023-09-13 19:20:00 +0200

    Add empty product page

---
+++ src/app/(with-navbar)/product/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage({ params: { productId } }: { params: { productId: string } }) {
+	return <>{productId}</>;
+}
--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { type Product } from "@types";
 
 type Props = {
@@ -8,19 +9,21 @@ type Props = {
 export const ProductItem = ({ product }: Props) => {
 	return (
 		<li key={product.id}>
-			<article className="group">
-				<div className="w-fit overflow-hidden rounded-lg bg-gray-200 p-0">
-					<Image
-						src={product.image}
-						alt={product.title}
-						width={320}
-						height={320}
-						className="aspect-square object-cover object-center group-hover:opacity-75"
-					/>
-				</div>
-				<h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
-				<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-			</article>
+			<Link href={`/product/${product.id}`}>
+				<article className="group">
+					<div className="w-fit overflow-hidden rounded-lg bg-gray-200 p-0">
+						<Image
+							src={product.image}
+							alt={product.title}
+							width={320}
+							height={320}
+							className="aspect-square object-cover object-center group-hover:opacity-75"
+						/>
+					</div>
+					<h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+					<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+				</article>
+			</Link>
 		</li>
 	);
 };
