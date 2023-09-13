25cbea5d5e7dd8dfa78d96d3710757c28505695a
Michał Grzelak <grzelakm@outlook.com>
2023-09-13 15:27:13 +0200

    General fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center">
-			<Link className="butto text-gray-900" href="/products">
+			<Link className="text-gray-900" href="/products">
 				Products
 			</Link>
 		</div>
