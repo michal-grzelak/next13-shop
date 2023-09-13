b78dfc90ea9fc7ca0f3412b548162b27ac0a9128
Michał Grzelak <grzelakm@outlook.com>
2023-09-13 16:04:31 +0200

    Add ActiveLink

---
+++ src/ui/ActiveLink/ActiveLink.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { type Route } from "next";
+import Link from "next/link";
+import { type ReactNode } from "react";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+
+export const ActiveLink = <T extends string>({
+	href,
+	children,
+}: {
+	href: Route<T> | URL;
+	children: ReactNode;
+}) => {
+	const pathname = usePathname();
+	const isActive = href === pathname;
+
+	return (
+		<Link
+			href={href}
+			className={clsx("text-blue-500 hover:underline", {
+				"text-blue-900": isActive,
+			})}
+		>
+			{children}
+		</Link>
+	);
+};
