4222dcb724d27e94160e99c759adcd11c2a2b1ca
Michał Grzelak <grzelakm@outlook.com>
2023-09-14 20:00:49 +0200

    Fix missing take

--- src/services/ProductService/ProductService.tsx
+++ src/services/ProductService/ProductService.tsx
@@ -1,9 +1,10 @@
 import { type Product } from "@types";
 
+const TAKE = 20;
 export class ProductService {
 	async getProducts({ page }: { page: number }): Promise<Product[]> {
-		const offset = (page - 1) * 20;
-		const params = new URLSearchParams({ offset: String(offset) });
+		const offset = (page - 1) * TAKE;
+		const params = new URLSearchParams({ offset: String(offset), take: String(TAKE) });
 
 		return fetch(`https://naszsklep-api.vercel.app/api/products?${params.toString()}`)
 			.then((res) => res.json() as Promise<Product[]>)
