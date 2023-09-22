c88f8bda4af28b6bfd4f2c77668ba9c4604d6b61
Michał Grzelak <grzelakm@outlook.com>
2023-09-22 19:10:41 +0200

    Add categories and collections pagination rewrite

--- next.config.js
+++ next.config.js
@@ -18,6 +18,16 @@ const nextConfig = {
 				destination: "/products/1",
 				permanent: false,
 			},
+			{
+				source: "/categories/:slug",
+				destination: "/categories/:slug/1",
+				permanent: false,
+			},
+			{
+				source: "/collections/:slug",
+				destination: "/collections/:slug/1",
+				permanent: false,
+			},
 		]
 	},
 }
